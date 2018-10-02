28d633eaaf96cc13a9b9a86d2396293e6aeeb716
Danny Warren <dannydwarren@gmail.com>
2018-10-01 20:50:55 -0600

    Post now saves shows and get return shows

--- index.js
+++ index.js
@@ -5,8 +5,26 @@ const port = 3003
 
 app.use(bodyParser.json())
 
-app.get('/tv-show', (req, res) => res.send('GET'))
-app.post('/tv-show', (req, res) => res.send('POST' + JSON.stringify(req.body)))
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
+    res.header('Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    next()
+})
+
+const tvShows = [{
+    name: 'Pizza Cat',
+    rating: 3,
+    imageUrl: 'https://caterville.files.wordpress.com/2013/10/fe0c8-pizza-cat.jpg'
+}]
+
+app.get('/tv-show', (req, res) => res.send(tvShows))
+app.post('/tv-show', (req, res) => {
+    console.log(req.body)
+    tvShows.push(req.body)
+    res.send({ success: true })
+})
 app.put('/tv-show', (req, res) => res.send('PUT' + JSON.stringify(req.body)))
 app.delete('/tv-show/:id', (req, res) => res.send('DELETE' + req.params.id))
 
