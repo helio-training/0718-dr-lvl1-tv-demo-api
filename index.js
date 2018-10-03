bf4968aa6913b55019457099b73d4a78f1b06cd4
Danny Warren <dannydwarren@gmail.com>
2018-10-02 19:11:17 -0600

    Use mongodb to store tvShows

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const db = require('monk')('mongodb://admin:password1@ds050077.mlab.com:50077/tv-demo')
 const app = express()
 const port = 3003
+const tvShowsCollection = db.get('tvShows')
 
 app.use(bodyParser.json())
 
@@ -13,19 +15,21 @@ app.use((req, res, next) => {
     next()
 })
 
-const tvShows = [{
-    name: 'Pizza Cat',
-    rating: 3,
-    imageUrl: 'https://caterville.files.wordpress.com/2013/10/fe0c8-pizza-cat.jpg'
-}]
+app.get('/tv-show', async (req, res) => {
+    const tvShows = await tvShowsCollection.find({})
+    res.send(tvShows)
+})
 
-app.get('/tv-show', (req, res) => res.send(tvShows))
 app.post('/tv-show', (req, res) => {
     console.log(req.body)
-    tvShows.push(req.body)
-    res.send({ success: true })
+    const savedTvShow = tvShowsCollection.insert(req.body)
+    res.send(savedTvShow)
 })
+
 app.put('/tv-show', (req, res) => res.send('PUT' + JSON.stringify(req.body)))
+
 app.delete('/tv-show/:id', (req, res) => res.send('DELETE' + req.params.id))
+
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
